b28414a620a18ea079fc8b3a3145a69c9ea1d597
Shawn Horvatic <sbhorvatic@gmail.com>
2017-05-25 20:50:54 -0500

    Add sa option to pick search area

--- imageProcessing/superSampling.go
+++ imageProcessing/superSampling.go
@@ -5,11 +5,11 @@ import (
 	"image"
 )
 
-func SuperSampling(source image.Image) image.Image {
+func SuperSampling(source image.Image, searchArea int) image.Image {
 	img := image.NewRGBA(source.Bounds())
 	for py := 0; py < img.Rect.Dy(); py++ {
 		for px := 0; px < img.Rect.Dx(); px++ {
-			img.Set(px, py, imagePixels.Average(imagePixels.Around(source, px, py)))
+			img.Set(px, py, imagePixels.Average(imagePixels.Around(source, px, py, searchArea)))
 		}
 	}
 	return img
